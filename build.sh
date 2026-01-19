a37956e7a76946c3301b87d53e132498bee614b3
pinkyj81 <yujin_pyj@daum.net>
2026-01-19 11:35:02 +0900

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-# Render 빌드 스크립트
+# Render 빌드 스크립트 - SQL Server 연결을 위한 시스템 패키지 설치
 
 set -o errexit
+
+# FreeTDS 및 의존성 설치 (pymssql 빌드에 필요)
+apt-get update
+apt-get install -y freetds-dev freetds-bin unixodbc-dev
 
 # Python 패키지 설치
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# SQL Server 2008 호환을 위한 시스템 패키지 설치 (선택사항)
-# apt-get update
-# apt-get install -y freetds-dev unixodbc-dev
 
 echo "빌드 완료!"
