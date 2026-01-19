67c3ab92d8324444e79b7fe01b7aa36a2c6eef42
pinkyj81 <yujin_pyj@daum.net>
2026-01-19 11:37:47 +0900

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-# Render 빌드 스크립트 - SQL Server 연결을 위한 시스템 패키지 설치
+# Render 빌드 스크립트
 
 set -o errexit
 
-# FreeTDS 및 의존성 설치 (pymssql 빌드에 필요)
-apt-get update
-apt-get install -y freetds-dev freetds-bin unixodbc-dev
-
-# Python 패키지 설치
+# Python 패키지 설치 (python-tds는 순수 Python이라 시스템 패키지 불필요)
 pip install --upgrade pip
 pip install -r requirements.txt
 
--- db_pymssql.py
+++ db_pymssql.py
@@ -1,5 +1,5 @@
 import os
-import pymssql
+import pytds
 
 def get_conn():
     server = os.getenv("DB_SERVER", "ms0501.gabiadb.com")
@@ -8,14 +8,15 @@ def get_conn():
     pw = os.getenv("DB_PASSWORD", "yj8630")
     port = int(os.getenv("DB_PORT", "1433"))
     
-    # pymssql은 SQL Server 2008과 호환성이 좋고 Render에서 잘 작동합니다
-    conn = pymssql.connect(
+    # python-tds는 순수 Python 구현으로 컴파일 불필요, SQL Server 2008 호환
+    conn = pytds.connect(
         server=server,
+        database=db,
         user=user,
         password=pw,
-        database=db,
         port=port,
         timeout=30,
-        login_timeout=30
+        login_timeout=30,
+        tds_version=pytds.TDS70  # SQL Server 2008 호환
     )
     return conn
